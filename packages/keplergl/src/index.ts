35571f0ad89ad5494595ae16d6f422700fb714b0
Xun Li <lixun910@gmail.com>
2025-05-05 19:58:20 -0700

    fix lint

--- packages/keplergl/src/index.ts
+++ packages/keplergl/src/index.ts
@@ -1,8 +1,5 @@
-export {
-  KeplerGlComponent,
-  KeplerGlComponentWithProvider,
-} from './map-layer/component/keplergl-component';
-
+export * from './map-layer/component/keplergl-component';
 export * from './map-layer/tool';
+
 export * from './types';
 export * from './register-tools';
